edcbaa584cce915c41cb2f2a7c3922c7c730cc8d
Roger Tan <teochewthunder@gmail.com>
2025-07-13 12:16:04 +0800

    More scaffolding

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require("express");
+
+var app = express();
+
+var handlebars = require("express-handlebars").create({defaultLayout:"main"});
+app.engine("handlebars", handlebars.engine);
+
+app.set("view engine", "handlebars");
+app.set("port", process.env.PORT || 3000);
+
+app.use(require("body-parser")());
+app.use(express.static("assets"));
+
+app.get("/", (req, res)=> {
+	res.render("form", { error: "", message: "Paste your text in the bo provied, then hit the PROCESS button." });
+});
+
+app.post("/process", (req, res)=> {
+	//use req.body
+});
+
+app.use((req, res, next)=> {
+	res.status(404);
+	res.render("404");
+});
+
+app.use((err, req, res, next)=> {
+	res.status(500);
+	res.render("500", { errorMessage: err.code });
+});
+
+app.listen(app.get("port"), ()=> {
+
+});
